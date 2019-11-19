57f2dc75de86d4d7237d7b251ed61b6724033663
adityagoyal <goyaladi>
2019-11-19 13:41:25 -0500

    All the files are working now in Phase 2 folder

--- phase2/app/src/main/java/com/example/game/Level1/GameManager.java
+++ phase2/app/src/main/java/com/example/game/Level1/GameManager.java
@@ -1,3 +1,154 @@
+//package com.example.game.Level1;
+//
+//import android.graphics.Canvas;
+//import android.graphics.Color;
+//import android.graphics.Paint;
+//import android.graphics.Typeface;
+//
+//import java.util.ArrayList;
+//
+//import static com.example.game.Level1.MainThread.canvas;
+//
+//public class GameManager {
+//
+//    protected static int gridHeight;
+//    protected static int gridWidth;
+//    protected ChildBall b;
+//    protected Button left;
+//    protected Button right;
+//    Paint paintText = new Paint();
+//    int score;
+//    boolean flag = false;
+//    String ballColor;
+//    int points;
+//    public static int finalScore=0;
+//
+//    ArrayList<Barrier> items = new ArrayList<Barrier>();
+//
+//    public GameManager(int height, int width, String ballColor, String points) {
+//        gridHeight = height;
+//        gridWidth = width;
+//        paintText.setTextSize(60);
+//        paintText.setTypeface(Typeface.DEFAULT_BOLD);
+//        this.ballColor = ballColor;
+//        if (points.equals("EASY"))
+//            this.points = 10;
+//        else
+//            this.points = 15;
+//        paintText.setColor(Color.WHITE);
+//
+//        score = 0;
+//    }
+//
+//    public void createItems()
+//    {
+//        Barrier b1 = new Barrier(0);
+//        items.add(b1);
+//        Barrier b2 = new Barrier(10);
+//        items.add(b2);
+//        Barrier b3 = new Barrier(20);
+//        items.add(b3);
+//        Barrier b4 = new Barrier(30);
+//        items.add(b4);
+//
+//        b = new ChildBall(15, 40, ballColor);
+//
+//        // button
+//        left = new Button(Level1view.leftButtonImage, 100, 1800);
+//        right = new Button(Level1view.rightButtonImage, 800, 1800);
+//
+//    }
+//
+//    public void update()
+//    {
+//        for(int i = 0; i<items.size();i++)
+//        {
+//            items.get(i).move();
+//            Barrier temp = items.get(items.size()-1);
+////            if(temp.height*Level1view.charHeight == 1680)
+//            if(temp.height == 39)
+//            {
+//                if(!temp.contains(b.x)) {
+//                    this.flag = true;
+//                    this.draw(Level1view.can);
+//                    collision();
+//                }
+//            }
+//        }
+//        delete_and_add_barrier();
+//    }
+//
+//    public void draw(Canvas canvas)
+//    {
+//
+//        if(flag)
+//        {
+//            canvas.drawText("YOU LOSE", 400, 800, paintText);
+//            Level1view.gameRunning = false;
+//        }
+//        else if(score < points) {
+//            canvas.drawText("SCORE:" + this.score, 60, 55, paintText);
+//        }
+//        if(score == points)
+//        {
+//            canvas.drawText("YOU WON", 400, 800, paintText);
+//            Level1view.gameRunning = false;
+//        }
+//
+//        for(int i = 0; i<items.size(); i++)
+//        {
+//            items.get(i).draw(canvas);
+//        }
+//        b.draw(canvas);
+//        left.draw(canvas);
+//        right.draw(canvas);
+//    }
+//
+//    public void delete_and_add_barrier()
+//    {
+//        for(int i=0;i<items.size();i++)
+//        {
+//            Barrier temp = items.get(i);
+//            if(temp.height > 40)
+//            {
+//                removeBarrier(temp);
+//                this.score += 1;
+//                finalScore += 1;
+//                add_barrier_at_top();
+//            }
+//        }
+//    }
+//
+//    private void removeBarrier(Barrier b)
+//    {
+//        items.remove(b);
+//    }
+//
+//    private void add_barrier_at_top()
+//    {
+//        int newBarrierHeight = items.get(0).height;
+//        newBarrierHeight -= 10;
+//        // Adds barrier at the top
+//        Barrier b = new Barrier(newBarrierHeight);
+//        items.add(0, b);
+//    }
+//
+//    public void buttonPressed(int x, int y)
+//    {
+//        if(left.contains(x, y)) {
+//            b.moveLeft();
+//        }
+//        else if(right.contains(x, y)) {
+//            b.moveRight();
+//        }
+//    }
+//
+//    public void collision()
+//    {
+////        Level1view.gameRunning = false;
+//        finalScore = score;
+//    }
+//}
 package com.example.game.Level1;
 
 import android.graphics.Canvas;
@@ -7,17 +158,18 @@ import android.graphics.Typeface;
 
 import java.util.ArrayList;
 
-import static com.example.game.Level1.MainThread.canvas;
+/**
+ * Manages how the game is run and displayed on the screen
+ * */
 
 public class GameManager {
 
     protected static int gridHeight;
     protected static int gridWidth;
-    protected ChildBall b;
+    protected Ball b;
     protected Button left;
     protected Button right;
     Paint paintText = new Paint();
-    int score;
     boolean flag = false;
     String ballColor;
     int points;
@@ -37,9 +189,11 @@ public class GameManager {
             this.points = 15;
         paintText.setColor(Color.WHITE);
 
-        score = 0;
     }
 
+    /**
+     * Creates the items displayed on the screen
+     */
     public void createItems()
     {
         Barrier b1 = new Barrier(0);
@@ -51,7 +205,7 @@ public class GameManager {
         Barrier b4 = new Barrier(30);
         items.add(b4);
 
-        b = new ChildBall(15, 40, ballColor);
+        b = new Ball(15, 40, ballColor);
 
         // button
         left = new Button(Level1view.leftButtonImage, 100, 1800);
@@ -59,25 +213,30 @@ public class GameManager {
 
     }
 
+    /**
+     * Updates the state of the game objects.
+     * */
     public void update()
     {
         for(int i = 0; i<items.size();i++)
         {
             items.get(i).move();
             Barrier temp = items.get(items.size()-1);
-//            if(temp.height*Level1view.charHeight == 1680)
-            if(temp.height == 39)
+            if(temp.getHeight() == 39)
             {
-                if(!temp.contains(b.x)) {
+                if(!temp.contains(b.getX())) {
                     this.flag = true;
                     this.draw(Level1view.can);
-                    collision();
                 }
             }
         }
-        delete_and_add_barrier();
+        deleteAndAddBarrier();
     }
 
+    /**
+     * Draws out the game objects on the canvas.
+     * @param canvas    where to draw the objects
+     */
     public void draw(Canvas canvas)
     {
 
@@ -86,10 +245,10 @@ public class GameManager {
             canvas.drawText("YOU LOSE", 400, 800, paintText);
             Level1view.gameRunning = false;
         }
-        else if(score < points) {
-            canvas.drawText("SCORE:" + this.score, 60, 55, paintText);
+        else if(finalScore < points) {
+            canvas.drawText("SCORE:" + finalScore, 60, 55, paintText);
         }
-        if(score == points)
+        if(finalScore == points)
         {
             canvas.drawText("YOU WON", 400, 800, paintText);
             Level1view.gameRunning = false;
@@ -104,35 +263,51 @@ public class GameManager {
         right.draw(canvas);
     }
 
-    public void delete_and_add_barrier()
+    /**
+     * Helper method to delete and add the barrier when gone from which have gone off the screen
+     */
+
+    private void deleteAndAddBarrier()
     {
         for(int i=0;i<items.size();i++)
         {
             Barrier temp = items.get(i);
-            if(temp.height > 40)
+            if(temp.getHeight() > 40)
             {
                 removeBarrier(temp);
-                this.score += 1;
                 finalScore += 1;
-                add_barrier_at_top();
+                addBarrierAtTop();
             }
         }
     }
 
+    /**
+     * Helper method to remove the barrier that have gone from the screen.
+     * @param b     barrier which has gone off the screen.
+     */
     private void removeBarrier(Barrier b)
     {
         items.remove(b);
     }
 
-    private void add_barrier_at_top()
+    /**
+     * Helper to add the barrier to the top of the screen
+     */
+    private void addBarrierAtTop()
     {
-        int newBarrierHeight = items.get(0).height;
+        int newBarrierHeight = items.get(0).getHeight();
         newBarrierHeight -= 10;
         // Adds barrier at the top
         Barrier b = new Barrier(newBarrierHeight);
         items.add(0, b);
     }
 
+    /**
+     * Responds to the button either left or right button pressed.
+     *
+     * @param x     the x coordinate of the tap
+     * @param y     the y coordinate of the tap
+     */
     public void buttonPressed(int x, int y)
     {
         if(left.contains(x, y)) {
@@ -141,11 +316,5 @@ public class GameManager {
         else if(right.contains(x, y)) {
             b.moveRight();
         }
-    }
-
-    public void collision()
-    {
-//        Level1view.gameRunning = false;
-        finalScore = score;
     }
 }
--- phase2/app/src/main/java/com/example/game/Level1/MainThread.java
+++ phase2/app/src/main/java/com/example/game/Level1/MainThread.java
@@ -1,13 +1,73 @@
+//package com.example.game.Level1;
+//import android.graphics.Canvas;
+//import android.graphics.Color;
+//import android.opengl.GLSurfaceView;
+//import android.view.MotionEvent;
+//import android.view.SurfaceHolder;
+//import android.view.SurfaceView;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//
+//public class MainThread extends Thread
+//{
+//
+//    private SurfaceHolder surfaceHolder;
+//    private Level1view gameView;
+//    private boolean isRunning;
+//    public static Canvas canvas;
+//
+//    public MainThread(SurfaceHolder surfaceHolder, Level1view gameView)
+//    {
+//        super();
+//        this.surfaceHolder = surfaceHolder;
+//        this.gameView = gameView;
+//
+//    }
+//
+//    public void setRunning(boolean isRunning)
+//    {
+//        this.isRunning = isRunning;
+//
+//    }
+//
+//    public void run()
+//    {
+//        while(isRunning)
+//        {
+//            canvas = null;
+//
+//            try {
+//                canvas = this.surfaceHolder.lockCanvas();
+//                synchronized (surfaceHolder) {
+//                    this.gameView.update();
+//                    this.gameView.draw(canvas);
+//
+//                }
+//            }
+//            catch(Exception e)
+//            {
+//            }
+//
+//            finally
+//            {
+//                if(canvas!=null)
+//                {
+//                    try{
+//                        surfaceHolder.unlockCanvasAndPost(canvas);
+//                    }
+//                    catch(Exception e)
+//                    {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 package com.example.game.Level1;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.opengl.GLSurfaceView;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-
 
 public class MainThread extends Thread
 {
